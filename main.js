e3183510872c65dc39f25a17d0d016f5020c1a54
Amal Hussein <amalhussein08@gmail.com>
2013-11-01 15:32:26 -0400

    pushing up class notes for events part 1

--- main.js
+++ main.js
@@ -10,22 +10,37 @@ var Page = {
 
 
 	setButtonEvent: function() {
+		Page.myButton.addEventListener('click', Page.clickResponse);
 		// write a function that adds an event listener to the button
 		// that will call the promptInfo function.
 		// after the button is clicked, it should say "change person info"
 	},
 
 	setLinkEvent: function() {
+		// we're defining a 'callback,' which is a function defined in-line
+		Page.myLink.addEventListener('click', function(event) { 
+			event.preventDefault();
+			Page.clickResponse;
+		});
 		// write a function that will add an event listener to the link,
 		// making sure that you account for the link's natural redirct.
 	},
 
+	linkClickResult: function (event) {
+
+	},
+
+	clickResponse: function() {
+		Page.promptInfo();
+		Page.changeButtonText();
+		Page.changeLinkText();
+	},
 
 	promptInfo: function() {
 		var name = prompt("what is your name?"),
 			age = prompt('how old are you?'),
 			email = prompt('what is your email');
-			addPerson(name, age, email);
+			Page.addPerson(name, age, email);
 	},
 
 	addPerson: function(name, age, email) {
@@ -35,9 +50,16 @@ var Page = {
 			emailField = "<h2>" + email + "</h2>";
 
 		userDiv.innerHTML = nameHeader + ageField	+ emailField;
+	}, 
+
+	changeButtonText: function() {
+			Page.myButton.innerHTML = 'Change User Info';
+	}, 
+
+	changeLinkText: function () {
+		Page.myLink.innerHTML = 'Change User Info'; 
 	}
 };
-
 
 Page.setButtonEvent();
 Page.setLinkEvent();
